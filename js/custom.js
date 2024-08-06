30920c22ac1c1867af973e398fb887fb25a8787d
MuhammadNashid <muhammadnashid905@gamail.com>
2024-08-06 16:07:53 +0530

    uploaded

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+// alert("external")
+
+// a=10
+// b=20
+// console.log(a=a+b);
+
+// a=10
+// b=30
+// c=10
+// console.log(a==b&&a==c);
+
+// a=10
+// b=20
+// c=10
+// console.log(a==b||a==c);
+
+
+// console.log(5+ "5");
+
+// console.log(3+ +"3"); 
+
+// console.log(4> 3);
+
+
+
+
+fno=parseFloat(prompt("Enter your first number"))
+
+sno=parseFloat(prompt("Enter your second number"))
+
+alert(fno+sno)
+
+
+
+
+// if(mark>90){
+//    alert("A+")
+// }
+// else if(mark>80){
+//     alert("A")
+// }
+// else if(mark>70){
+//     alert("B+")
+// }
+// else if(mark>60){
+//     alert("B")
+// }
+// else if(mark>50){
+//     alert("C+")
+// }
+// else if(mark>40){
+//     alert("C")
+// }
+// else if(mark>30){
+//     alert("D+");
+// }
+// else{
+//     alert("fail");
+// }
