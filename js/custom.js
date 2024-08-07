2a8649ca4a177bb7f7f7e07f4ebeefc72bc2fd8a
MuhammadNashid <muhammadnashid905@gmail.com>
2024-08-07 16:06:17 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -24,11 +24,11 @@
 
 
 
-fno=parseFloat(prompt("Enter your first number"))
+// fno=parseFloat(prompt("Enter your first number"))
 
-sno=parseFloat(prompt("Enter your second number"))
+// sno=parseFloat(prompt("Enter your second number"))
 
-alert(fno+sno)
+// alert(fno+sno)
 
 
 
@@ -57,3 +57,66 @@ alert(fno+sno)
 // else{
 //     alert("fail");
 // }
+
+
+// op=10
+
+// switch(op){
+//     case 1: console.log("Sunday");
+//     break;
+//     case 2: console.log("Monday");
+//     break;
+//     case 3: console.log("Tuesday");
+//     break;
+//     case 4: console.log("Wednesday");
+//     break;
+//     case 5: console.log("Thursday");
+//     break;
+//     case 6: console.log("Friday");
+//     break;
+//     case 7: console.log("Saturday");
+//     break;
+//     default:console.log("Invalid");
+// }
+
+
+// i=1
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+
+
+// i=1
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10){
+
+// }
+
+// for(i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+
+// for(i=1;i<=10;i=i+2)
+// {
+//     console.log(i)
+// }
+
+
+// even
+
+// for(i=2;i<=10;i=i+2)
+// {
+//     console.log(i);
+// }
+
+
+for(i=1;i<=100;i++){
+    console.log(i);
+    
+}
