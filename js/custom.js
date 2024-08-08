4b0a319719b9678479210f2b2fc5705cf6476c01
MuhammadNashid <muhammadnashid905@gmail>
2024-08-08 16:05:37 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -116,7 +116,122 @@
 // }
 
 
-for(i=1;i<=100;i++){
-    console.log(i);
+// for(i=1;i<=100;i++){
+//     console.log(i);
+//     if(i%3==0)
+//      console.log('fizz');
     
+    
+// }
+
+// for(i=1;i<=100;i++){
+//     console.log(i);
+//     if(i%5==0)
+//      console.log('buzz');
+//     else if(i%3==0)
+//     console.log('fizz');    
+// }
+
+
+// for(i=1;i<=100;i++){
+//     console.log(i);
+//     if(i%15==0)
+//      console.log('fizzbuzz');
+//      if(i%3==0)
+//     console.log('fizz');
+//     else if(i%5==0)
+//     console.log('buzz');
+// }
+
+
+
+// for(i=1;i<=10;i++)
+// {
+   
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// for(i=1;i<=10;i++)
+// {
+//     if(i==5)
+//     {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+// function asd(){
+
+//     if(true){
+//         let a=30
+//         var b=10
+//         console.log("inside block"+a);
+//     }
+//     console.log("inside function"+b);
+
+// }
+// asd()
+
+
+// for(i=1;i<=5;i++)
+// {
+//     str=""
+
+//    for(j=1;j<=5;j++)
+//    {
+//     str+=j;
+//    }
+//    console.log(str);
+// }
+
+// for(i=1;i<=5;i++)
+// {
+//     str=""
+
+//    for(j=1;j<=i;j++)
+//    {
+//     str+=j;
+//    }
+//    console.log(str);
+// }
+
+// for(i=1;i<=5;i++)
+// {
+//     str=""
+
+//    for(j=1;j<=i;j++)
+//    {
+//     str+="*";
+//    }
+//    console.log(str);
+// }
+
+
+// for(i=1;i<=5;i++)
+// {
+//     str=""
+
+//    for(j=1;j<=5;j++)
+//    {
+//     str+=j;
+//    }
+//    console.log(str);
+// }
+
+
+for(i=1;i<=5;i++)
+{
+    str=""
+
+   for(j=1;j<=i;j++)
+   {
+    str+=j;
+   }
+   console.log(str);
 }
