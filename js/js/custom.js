cd68e2efd6b893917beaf5750adf61142769e9f7
MuhammadNashid <muhammadnashid905@gmail.com>
2024-08-20 15:18:29 +0530

    updated

--- js/js/custom.js
+++ js/js/custom.js
@@ -709,16 +709,31 @@
 
 i=1
 
+// function asd(){
+//     setTimeout(()=>{
+//         document.write("*")
+//         if(i<=10){
+//             asd()
+//         }
+//         i++
+//     },1000)
+
+// } 
+
+// asd()
+
+function putZero(sec){
+    return sec<10?`0${sec}`: sec;
+}
+
 function asd(){
-    setTimeout(()=>{
-        document.write("*")
-        if(i<=10){
-            asd()
-        }
-        i++
-    },1000)
-
-} 
-
+let dt=new Date();
+let hours= dt.getHours()
+let ampm=hours>12?"PM":"AM";
+document.getElementById("asd").innerHTML=`${putZero(hours%12)} : ${putZero(dt.getMinutes())} : ${putZero(dt.getSeconds())} ${ampm}`
+setTimeout(()=>{
+         asd() 
+},1000)
+}  
 asd()
 
