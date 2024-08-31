c7a1c18e5e022b12dc771d5e580fb8103412fdfe
MuhammadNashid <muhammadnashid905@gmail.com>
2024-08-31 16:45:43 +0530

    file updated

--- js/js/Task2.js
+++ js/js/Task2.js
@@ -50,21 +50,48 @@
 
         //COMMENTS//
 
+        // function fetchData(){
+        //     fetch("https://jsonplaceholder.typicode.com/comments")
+        //     .then((res)=>{
+        //         return res.json();
+        //     })
+        //     .then((data)=>{
+        //         console.log(data);
+        //         str=``
+        //         data.map((dt)=>{
+        //             str+=`<tr>
+        //             <td>${dt.id}</td>
+        //             <td>${dt.name}</td>
+        //             <td>${dt.email}</td>
+        //             <td>${dt.body}</td>
+        //         </tr>`;
+        //         })
+        //         document.getElementById("data").innerHTML=str
+        //     })
+        //     .catch((error)=>{
+        //         console.log(error);
+                
+        //     })
+        
+        // }
+
+
+
         function fetchData(){
-            fetch("https://jsonplaceholder.typicode.com/comments")
+            fetch("https://jsonplaceholder.typicode.com/albums")
             .then((res)=>{
                 return res.json();
             })
             .then((data)=>{
-                console.log(data);
+
+
                 str=``
-                data.map((dt)=>{
-                    str+=`<tr>
-                    <td>${dt.id}</td>
-                    <td>${dt.name}</td>
-                    <td>${dt.email}</td>
-                    <td>${dt.body}</td>
-                </tr>`;
+                data.map((content)=>{
+                    str+=`
+                    <div class="asd">${content.userId}<br>
+                    ${content.id}<br>
+                    ${content.title}
+                </div>`;
                 })
                 document.getElementById("data").innerHTML=str
             })
