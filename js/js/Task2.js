c5f31772d518b7c272ca6edb148e5110134f32a3
MuhammadNashid <muhammadnashid905@gmail.com>
2024-09-02 16:02:08 +0530

    updated

--- js/js/Task2.js
+++ js/js/Task2.js
@@ -77,27 +77,89 @@
 
 
 
-        function fetchData(){
-            fetch("https://jsonplaceholder.typicode.com/albums")
-            .then((res)=>{
-                return res.json();
-            })
-            .then((data)=>{
+        // function fetchData(){
+        //     fetch("https://jsonplaceholder.typicode.com/albums")
+        //     .then((res)=>{
+        //         return res.json();
+        //     })
+        //     .then((data)=>{
 
 
-                str=``
-                data.map((content)=>{
-                    str+=`
-                    <div class="asd">${content.userId}<br>
-                    ${content.id}<br>
-                    ${content.title}
-                </div>`;
-                })
-                document.getElementById("data").innerHTML=str
-            })
-            .catch((error)=>{
-                console.log(error);
+        //         str=``
+        //         data.map((content)=>{
+        //             str+=`
+        //             <div class="asd">${content.userId}<br>
+        //             ${content.id}<br>
+        //             ${content.title}
+        //         </div>`;
+        //         })
+        //         document.getElementById("data").innerHTML=str
+        //     })
+        //     .catch((error)=>{
+        //         console.log(error);
                 
-            })
+        //     })
         
-        }
+        // }
+
+
+        // function fetchData(){
+        //     fetch("https://jsonplaceholder.typicode.com/photos")
+        //     .then((res)=>{
+        //         return res.json();
+        //     })
+
+        //     .then((data)=>{
+
+
+        //        str=`` 
+
+        //        data.map((content)=>{
+        //         str+=`
+        //         <div class="photos">
+        //         <img src="${content.url}" alt="" style="width: 350px;" "height: 200px;"><br>
+        //         <div class="tle">
+        //         ${content.title}</div>
+        //         </div>`;
+        //     })
+        //     document.getElementById("data").innerHTML=str
+        //     })
+        //     .catch((error)=>{
+        //         console.log(error);
+                
+        //     })
+        // }
+
+
+
+        // TODOS//
+
+
+        function fetchData(){
+                fetch("https://jsonplaceholder.typicode.com/todos")
+                .then((res)=>{
+                    return res.json();
+                   
+                })
+                .then((data)=>{
+                    console.log(data);
+                    str=``
+                    
+                    data.map((dt)=>{
+                        str+=`<tr>
+                        <td>${dt.id}</td>
+                        <td>${dt.userId}</td>
+                        <td>${dt.title}</td>
+                        <td>${dt.completed}</td>
+                    </tr>`;
+                    })
+                    document.getElementById("data").innerHTML=str
+                })
+
+                .catch((error)=>{
+                    console.log(error);
+                    
+                })
+            
+            }
+        
